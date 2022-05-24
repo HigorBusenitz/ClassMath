977a49d3768eaf82a0e50ec14cdb995eb1d12055
Jeferson Eduardo Guido <jefersoneduardoguido@gmail.com>
2022-05-24 20:13:24 -0300

    Removido o serviço que estava causando erro na execução da empresa.

--- rest-siren-ta/src/main/java/br/edu/utfpr/rest_biblioteca/repositories/LogRepository.java
+++
@@ -1,8 +0,0 @@
-package br.edu.utfpr.rest_biblioteca.repositories;
-
-import br.edu.utfpr.rest_biblioteca.domain.Calculos;
-import br.edu.utfpr.rest_biblioteca.domain.Log;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LogRepository extends JpaRepository<Log, String> {
-}
